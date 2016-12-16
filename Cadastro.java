3d6836357fdf84ce378685a8fee5917318429aec
Rodolfo Mathias <rodolfo_psl@hotmail.com>
2016-12-16 18:15:48 -0200

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,53 @@
+package projeto.domain;
+
+public class Cadastro {
+
+	private int codigo;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String cidade;
+	private String profissao;
+	
+	/* gets and sets */
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+	
+
+}
